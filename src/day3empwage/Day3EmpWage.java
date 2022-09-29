712c37b0b6a2763aaf51c54e265d17c90329cbf4
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-29 13:42:38 +0530

    Added: UC1 checking attendance

--- src/day3empwage/Day3EmpWage.java
+++ src/day3empwage/Day3EmpWage.java
@@ -1,9 +1,19 @@
 package day3empwage;
 
+import java.util.Random;
+
 public class Day3EmpWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Emp Wage!");
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        int PRESENT = 1;
+        
+        if (attendance == PRESENT)
+            System.out.println("Employee is Present!");
+        else
+            System.out.println("Employee is Absent!");
         
     }
     
