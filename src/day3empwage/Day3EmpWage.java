f9ef1385d3097caecce42ee72dbfbbeb87a09d61
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-29 15:01:45 +0530

    Refactored: UC4 Solve using switch

--- src/day3empwage/Day3EmpWage.java
+++ src/day3empwage/Day3EmpWage.java
@@ -4,30 +4,33 @@ import java.util.Random;
 
 public class Day3EmpWage {
 
+    static final int PART_TIME = 1;
+    static final int FULL_TIME = 2;
+
     public static void main(String[] args) {
-        
+
         System.out.println("Welcome to Emp Wage!");
         Random random = new Random();
         int attendance = random.nextInt(3);
-        int PART_TIME = 1;
-        int FULL_TIME = 2;
+
         int WAGE_PER_HOUR = 20;
         int fullDayHour = 0;
         int dailyWage = 0;
-        
-        if (attendance == PART_TIME) {
-            fullDayHour = 4;
-            System.out.println("Employee is Parttime!");
+
+        switch (attendance) {
+            case PART_TIME:
+                fullDayHour = 4;
+                System.out.println("Employee is Parttime!");
+                break;
+            case FULL_TIME:
+                fullDayHour = 8;
+                System.out.println("Employee is Fulltime!");
+                break;
+            default:
+                System.out.println("Employee is Absent!");
         }
-        else if (attendance == FULL_TIME) {
-            fullDayHour = 8;
-            System.out.println("Employee is Fulltime!");
-        }
-        else
-            System.out.println("Employee is Absent!");
-        
         dailyWage = WAGE_PER_HOUR * fullDayHour;
         System.out.println("Daily wage of employee: " + dailyWage);
     }
-    
+
 }
